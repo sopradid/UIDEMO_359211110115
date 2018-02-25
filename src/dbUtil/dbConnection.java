14ebd2b5a93de122dd459641ef72122e076c9189
sopradid <may14082538@gmail.com>
2018-02-25 17:16:53 +0700

    Save Connection iogin

--- src/dbUtil/dbConnection.java
+++ src/dbUtil/dbConnection.java
@@ -2,10 +2,12 @@ package dbUtil;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class dbConnection {
     private static final String SQCONN = "jdbc:sqlite:school.sqlite";
-    public static Connection getConnection(){
+
+    public static Connection getConnection() throws SQLException {
         try {
             Class.forName("org.sqlite.JDBC");
             return DriverManager.getConnection(SQCONN);
@@ -14,4 +16,10 @@ public class dbConnection {
         }
 
         return null;
+
+    }
+
+
+
+
 }//class
--- src/sample/loginModel.java
+++ src/sample/loginModel.java
@@ -1,18 +1,54 @@
 package sample;
 
+import dbUtil.dbConnection;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class loginModel {
-    public class loginModel{
-        Connection connection
 
-        public loginModel() {
+    Connection connection;
+
+    public loginModel() {
         try {
-            this.connection =
+            this.connection = dbConnection.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-    }
+
+        if (this.connection == null) {
+            System.exit(1);
+        }
+    }//loginModel
+    public boolean isDatabaseConnection(){
+        return this.connection != null;
+    }//isDatabaseConnection
+
+    public boolean isLogin(String user, String pass) throws SQLException {
+        PreparedStatement pr = null;
+        ResultSet rs = null;
+
+        String sql = "select * from user where username = ? and password = ?";
+        try {
+            pr = this.connection.prepareStatement(sql);
+            pr.setString(1,user);
+            pr.setString(2, pass);
+            rs = pr.executeQuery();
+
+            if (rs.next()) {
+                return true;
+            }
+            return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }finally {
+            pr.close();
+            rs.close();
+        }
 
 
-
-
-}///cless
+    }//isLogin
+}//class
